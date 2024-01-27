ba00f80baef4ce672cff45beb60c78f1d820be73
NuminousCode <sanchezwebdev@gmail.com>
2024-01-27 05:33:29 -0600

    Image component mod

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -27,7 +27,7 @@ const Hero = (data) => {
       </div>
       <div className = {styles.slogan}>MODERN WEB DEVELOPMENT</div>
       <Menu />
-      <Image src={imgUrl} alt="image" className = {styles.image} width={11792} height={12137}/>
+    <Image src={imgUrl} alt="image" className = {styles.image} width={2000} height={1000}/>
     </div>
   )
 }
