98fcdde87ab6cc8382bd606fdbe552dc7f2438ba
NuminousCode <sanchezwebdev@gmail.com>
2024-04-21 12:51:11 -0500

    added conditional load for hero section

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,13 +1,14 @@
 import React from 'react'  
+import { useState, useEffect } from 'react'
 import Menu from './Menu'
 import Head from 'next/head'
 import styles from '../styles/Hero.module.css'
-import useDynamicIconSize from '../helpers/dynamicIconSize'
 import useDynamicFontSize from '../helpers/dynamicFontSize'
 import useDynamicLineHeight from '../helpers/dynamicLineHeight'
 
 const Hero = (data) => {
 
+  const [imageLoaded, setImageLoaded] = useState(false)
   // Image URL extraction
   const imgUrl = data.data[0]
 
@@ -19,12 +20,28 @@ const Hero = (data) => {
   useDynamicFontSize([`.${styles.slogan}`], 20, 100, 0.052);
   useDynamicLineHeight([`.${styles.slogan}`], 0.03);
 
+  useEffect(() => {
+    const handleImageLoad = () => {
+      setImageLoaded(true);
+    };
+
+    const image = new Image();
+    image.onload = handleImageLoad;
+    image.src = formatedCmsUrl;
+
+    return () => {
+      image.onload = null;
+    };
+  }, [formatedCmsUrl]);
+
+  
   return (
     <>
       <Head>
         <link rel="preload" href={formatedCmsUrl} as="image" />
         <link rel="preload" href="../styles/Hero.module.css" as="style" />
       </Head>
+      {imageLoaded &&
       <div className={styles.containerMain}>
         <div className={styles.header}>
           <div className={styles.logo}>G.S</div>
@@ -33,6 +50,7 @@ const Hero = (data) => {
         <Menu />
         <img src={formatedCmsUrl} alt="image" className={styles.image} />
       </div>
+      }
     </>
   );
 }
